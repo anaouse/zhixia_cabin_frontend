d6aa4dfa4036561b55374d86a4c3bdefeb00a07d
anaouse <weihu123@protonmail.com>
2026-04-03 22:46:48 +0800

    update taobao token

--- src/content/ShopContent.jsx
+++ src/content/ShopContent.jsx
@@ -3,6 +3,6 @@ export const shopItems = [
     imageSrc: '/zhixia_cabin_taobao.jpg',
     imageAlt: '徵夏小屋淘宝店',
     buttonLabel: '点击此按钮复制淘口令后打开淘宝自动进入小店',
-    taobaoToken: 'https://m.tb.cn/h.ikcI2aqwSbyZK8K',
+    taobaoToken: "【淘宝】07《Fqpx5dyH8Yy£ https://m.tb.cn/h.ikcI2aqwSbyZK8K  CZ007 我分享给你了一个超赞的内容，快来看看吧 \n点击链接直接打开 或者 淘宝搜索直接打开",
   },
 ]
